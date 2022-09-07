e6bbcf22c97be538bbdea49ead0e9bbd754ae92d
Sanjib Kumar Mandal <sanjib0248@gmail.com>
2022-09-07 20:42:31 +0530

    publish

--- lib/authentication.module.d.ts
+++ lib/authentication.module.d.ts
@@ -4,7 +4,7 @@ import * as i0 from "@angular/core";
 import * as i1 from "./authentication.tag";
 import * as i2 from "@angular/common";
 export declare class GoogleIdentityModule {
-    static init(authConfig: AuthInitialConfiguration): ModuleWithProviders<any>;
+    static init(authConfig: AuthInitialConfiguration): ModuleWithProviders<GoogleIdentityModule>;
     static ɵfac: i0.ɵɵFactoryDeclaration<GoogleIdentityModule, never>;
     static ɵmod: i0.ɵɵNgModuleDeclaration<GoogleIdentityModule, [typeof i1.AuthenticationTag], [typeof i2.CommonModule], [typeof i1.AuthenticationTag]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<GoogleIdentityModule>;
